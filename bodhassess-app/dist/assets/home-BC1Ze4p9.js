bf17a45b7efdd0e61de798f47e78f845acfba7d6
Utkrisht <Utkrisht@easylearning.guru>
2026-05-11 13:50:07 +0530

    updated front end

---
+++ bodhassess-app/dist/assets/home-BC1Ze4p9.js
@@ -0,0 +1,1 @@
+import{j as a,N as e}from"./index-DW_ot52I.js";function r(){return a.jsx(e,{to:"/dashboard",replace:!0})}export{r as default};
---
+++ bodhassess-app/dist/assets/library-Dr6is5ha.js
@@ -0,0 +1,1 @@
+import{o as a}from"./index-DW_ot52I.js";const e=[["path",{d:"m16 6 4 14",key:"ji33uf"}],["path",{d:"M12 6v14",key:"1n7gus"}],["path",{d:"M8 8v12",key:"1gg7y9"}],["path",{d:"M4 4v16",key:"6qkkli"}]],t=a("library",e);export{t as L};
