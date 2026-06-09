5211852cda29c85ce68f5b24a04956417c19582d
isthatdhruv <dhruv.kccsw@kccitm.edu.in>
2026-06-09 13:22:41 +0530

    build

---
+++ bodhassess-app/dist/assets/clock-2MRIHvr1.js
@@ -0,0 +1,1 @@
+import{at as c}from"./index-ByPEYDtB.js";const o=[["path",{d:"M12 6v6l4 2",key:"mmk7yg"}],["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}]],t=c("clock",o);export{t as C};
---
+++ bodhassess-app/dist/assets/home-lrBq70dI.js
@@ -0,0 +1,1 @@
+import{aL as a,T as e}from"./index-ByPEYDtB.js";function r(){return a.jsx(e,{to:"/dashboard",replace:!0})}export{r as default};
