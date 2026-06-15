0d9d29e83222488ceb088729c94f33ee76dea80f
isthatdhruv <dhruv.kccsw@kccitm.edu.in>
2026-06-15 16:22:27 +0530

    build

--- bodhassess-app/dist/assets/home-BFB2Hinw.js
+++
@@ -1,1 +0,0 @@
-import{aL as a,T as e}from"./index-9y771ScY.js";function r(){return a.jsx(e,{to:"/dashboard",replace:!0})}export{r as default};
---
+++ bodhassess-app/dist/assets/home-CSsF2RVO.js
@@ -0,0 +1,1 @@
+import{aL as a,T as e}from"./index-BO4eWEj8.js";function r(){return a.jsx(e,{to:"/dashboard",replace:!0})}export{r as default};
